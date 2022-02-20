5421678f9a44c86651b19ffb2387c07b85720d5e
emiliocaraballo <emiliocaraballo9810@gmail.com>
2022-02-20 10:08:51 -0500

    v1.0.0

--- src/lib/extra.ts
+++ src/lib/extra.ts
@@ -37,5 +37,28 @@ class Extra{
         var now = moment(new Date(date));
         return now.format("YYYY-MM-DD HH:mm:ss");
     }
+
+    // obtener infomacion de sistema operativo y navegador de forma detallada.
+    public UserAgent=(uastring1:string | undefined)=>{
+        var ua = parser(uastring1);
+        return {browser:ua.browser.name,browserversion:ua.browser.version,sistema:ua.os};
+    }
+    public string_to_slug=(str:string)=>{
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+      
+        // remove accents, swap ñ for n, etc
+        var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+        var to   = "aaaaeeeeiiiioooouuuunc------";
+        for (var i=0, l=from.length ; i<l ; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+    
+        str = str.replace(/[^a-z0-9. -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+    
+        return str;
+    }
 }
 export const extra = new Extra;
